3408c9d1484c6232d9b9d25e5f3d214d913d9e99
Laura Tannahill <lntannahill@gmail.com>
2021-01-11 00:15:23 +0000

    Implement add new image feature

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,25 @@ class PhotosController < ApplicationController
   def search
     @photos = Photo.where("category ILIKE ?", "%" + params[:q] + "%")
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to [:photos], notice: 'Image added!'
+    else
+      render :new
+    end
+  end
+
+  def photo_params
+    params.require(:photo).permit(
+      :category,
+      :image_url
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'photos/index'
   root 'photos#index'
-
-  get "search", to: "photos#search"
+  resources :photos, only: [:index, :new, :create :search]
 
 end
